84e3b0ee776cdb1020207240079ac91a653871f6
Kelly Munro <kellymdev@gmail.com>
2015-07-29 11:35:08 +1200

    Initial seed data for location descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,38 @@
 locations = Location.create([
                               {
-                                name: "Kapiti Island"
+                                name: "Kapiti Island",
+                                description: "A predator-free island off the Kapiti Coast. Home to several species of native forest bird."
                               },
                               {
-                                name: "Makara Beach"
+                                name: "Makara Beach",
+                                description: "A beach on Wellington's rugged south-west coast."
                               },
                               {
-                                name: "Mana Island"
+                                name: "Mana Island",
+                                description: "A predator-free island off the Plimmerton coast."
                               },
                               {
-                                name: "Pauatahanui Wildlife Reserve"
+                                name: "Pauatahanui Wildlife Reserve",
+                                description: "A coastal wetland, home to several species of sea bird."
                               },
                               {
-                                name: "Plimmerton Beach"
+                                name: "Plimmerton Beach",
+                                description: "A rocky beach on the Porirua coast."
                               },
                               {
-                                name: "Staglands Wildlife Reserve"
+                                name: "Staglands Wildlife Reserve",
+                                description: "Home to several species of farm animal."
                               },
                               {
-                                name: "Waikanae Estuary"
+                                name: "Waikanae Estuary",
+                                description: "A coastal lagooon and wetland area. Home to several species of sea bird."
                               },
                               {
-                                name: "Wellington Zoo"
+                                name: "Wellington Zoo",
+                                description: "Home to several exotic species."
                               },
                               {
-                                name: "Zealandia"
+                                name: "Zealandia",
+                                description: "A predator-fenced wildlife and bird sanctuary, 10 minutes drive from the Wellington CBD."
                               }
                             ])
