4c0b8c855dfde6dff85e1d47f8d22d3a81ea98b5
Kelly Munro <kellymdev@gmail.com>
2015-07-30 09:57:14 +1200

    Added method to return species for a location

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :species, only: [:index, :show]
   resources :locations, only: [:index, :show]
+
+  get 'locations/:id/species', to: 'locations#species', as: :location_species
 end
