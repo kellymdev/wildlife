72f7e3a2e258114dff1b2dab20f9008b48161ac9
Kelly Munro <kellymdev@gmail.com>
2015-07-30 14:40:36 +1200

    Add pages controller and home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   get 'locations/search/:query', to: 'locations#search', as: :location_search
   get 'locations/:id/species', to: 'locations#species', as: :location_species
 
