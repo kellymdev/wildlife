7d9e04e6ff0b647a9d6de9a198ea23ee7f7679ef
Kelly Munro <kellymdev@gmail.com>
2020-02-15 15:55:00 +1300

    Upgrade to Rails 6

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source 'https://rubygems.org'
-ruby '2.6.1'
+ruby '2.7.0'
 
-gem 'rails', '~> 5.2.2'
+gem 'rails', '~> 6.0.0'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 4.1'
-gem 'coffee-rails', '~> 4.2'
+gem 'coffee-rails', '~> 5.0'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
 gem 'jquery-turbolinks'
@@ -16,19 +16,20 @@ gem 'normalize-rails'
 
 group :development do
   gem 'web-console'
+  gem 'listen'
 end
 
 group :development, :test do
   gem 'byebug'
   gem 'spring'
-  gem 'rspec-rails'
+  gem 'rspec-rails', '~> 4.0.0.beta2'
   gem 'factory_bot_rails'
   gem 'jasmine'
 end
 
 group :test do
   gem 'capybara'
-  gem 'chromedriver-helper'
+  gem 'webdrivers'
   gem 'selenium-webdriver'
   gem 'shoulda-matchers'
   gem 'simplecov', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -1,56 +1,55 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812075255) do
+ActiveRecord::Schema.define(version: 2020_02_15_023427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "locations", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-    t.integer  "region_id"
-    t.decimal  "lat",         precision: 10, scale: 6
-    t.decimal  "lng",         precision: 10, scale: 6
+  create_table "locations", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "region_id"
+    t.decimal "lat", precision: 10, scale: 6
+    t.decimal "lng", precision: 10, scale: 6
   end
 
-  create_table "regions", force: :cascade do |t|
-    t.string   "name"
+  create_table "regions", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "species", force: :cascade do |t|
-    t.string   "name"
-    t.string   "scientific_name"
-    t.string   "maori_name"
-    t.string   "description"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "id_male"
-    t.string   "id_female"
-    t.string   "id_juvenile"
-    t.string   "image_url"
-    t.string   "image_caption"
+  create_table "species", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "scientific_name"
+    t.string "maori_name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "id_male"
+    t.string "id_female"
+    t.string "id_juvenile"
+    t.string "image_url"
+    t.string "image_caption"
   end
 
-  create_table "species_locations", force: :cascade do |t|
-    t.integer  "species_id"
-    t.integer  "location_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "species_locations", id: :serial, force: :cascade do |t|
+    t.integer "species_id"
+    t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
